0d306b763d94c922a349e34c30a369eb2cbbd6a6
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-14 03:10:39 +0900

    Add functions

--- packages/stream-util/src/index.ts
+++ packages/stream-util/src/index.ts
@@ -7,7 +7,54 @@ export { JSON, Lines };
 
 const debug = util.debuglog('@moneyforward/stream-util');
 
-export async function stringify(readable: stream.Readable): Promise<string> {
+export type Mapper<T, U = T> = (value: T, index: number) => U | PromiseLike<U>;
+export async function* map<T, U = T>(asyncIterable: AsyncIterable<T>, mapper: Mapper<T, U>): AsyncIterable<U> {
+  let index = -1;
+  for await (const value of asyncIterable) yield mapper(value, index += 1);
+}
+
+export type Predicate<T> = (value: T, index: number) => boolean;
+export async function* filter<T>(asyncIterable: AsyncIterable<T>, predicate: Predicate<T>): AsyncIterable<T> {
+  let index = -1;
+  for await (const value of asyncIterable) if (predicate(value, index += 1)) yield value;
+}
+
+export type Reducer<T, U = T> = (previous: U, current: T, index: number) => U;
+export async function reduce<T, U = T>(asyncIterable: AsyncIterable<T>, reducer: Reducer<T, U>, initValue?: U): Promise<U> {
+  const iterator = asyncIterable[Symbol.asyncIterator]();
+  let next;
+  if ((next = await iterator.next()).done) {
+    if (initValue === undefined) {
+      throw new TypeError('Reduce of empty array with no initial value');
+    } else {
+      return initValue;
+    }
+  }
+  let index = 0;
+  let previous: U = initValue === undefined ? next.value : reducer(initValue, next.value, index);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    if ((next = await iterator.next()).done) return previous;
+    previous = reducer(previous, next.value, index += 1);
+  }
+}
+
+export async function first<T>(asyncIterable: AsyncIterable<T>): Promise<T | undefined> {
+  for await (const value of asyncIterable) return value;
+}
+
+export async function count<T = unknown>(asyncIterable: AsyncIterable<T>): Promise<number> {
+  return reduce(asyncIterable, previous => previous + 1, 0);
+}
+
+export async function arrayify<T>(asyncIterable: AsyncIterable<T>): Promise<T[]> {
+  return reduce<T, T[]>(asyncIterable, (array, value) => {
+    array.push(value);
+    return array;
+  }, [])
+}
+
+async function _stringify(readable: stream.Readable): Promise<string> {
   const isObjectStream = (readable: stream.Readable): boolean => readable.readableObjectMode;
   const isTextStream = (readable: stream.Readable): boolean => {
     interface Readable extends stream.Readable {
@@ -25,12 +72,18 @@ export async function stringify(readable: stream.Readable): Promise<string> {
   };
 
   if (isObjectStream(readable) || isTextStream(readable)) {
-    let text = '';
-    for await (const chunk of readable) text += String(chunk);
-    return text;
+    return reduce(readable, (previous, current) => previous + String(current), '');
   } else {
-    const buffers: Buffer[] = [];
-    for await (const chunk of readable) buffers.push(chunk);
-    return Buffer.concat(buffers).toString();
+    const asyncIterable: AsyncIterable<Buffer> = readable;
+    return Buffer.concat(await arrayify(asyncIterable)).toString();
   }
+}
+
+export async function stringify<T = unknown>(asyncIterable: AsyncIterable<T>): Promise<string> {
+  if (asyncIterable instanceof stream.Readable) return _stringify(asyncIterable);
+  return reduce(asyncIterable, (previous, current) => previous + String(current), '');
+}
+
+export async function* of<T>(...values: T[]): AsyncIterable<T> {
+  yield* values;
 }
