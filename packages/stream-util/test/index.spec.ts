1ec76cfba057e207dcff007f4da17e86ec4d9fa6
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-14 03:10:51 +0900

    Add tests

--- packages/stream-util/test/index.spec.ts
+++ packages/stream-util/test/index.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import stream from 'stream';
-import { stringify } from '../src/index';
+import { arrayify, stringify, reduce, map, of } from '../src/index';
 
 describe('stringify', () => {
   it('should return a string', async () => {
@@ -21,6 +21,25 @@ describe('stringify', () => {
     const expected = 'hello, world!';
     const readable = stream.Readable.from(Buffer.from(expected), { objectMode: false });
     const actual = await stringify(readable);
+    expect(actual).to.equal(expected);
+  });
+});
+
+describe('map', () => {
+  it('should return new iterable', async () => {
+    const numbers = [1, 2, 3];
+    const expected = numbers.map(String);
+    const actual = map(of(...numbers), async value => String(value));
+    expect(await arrayify(actual)).to.deep.equal(expected);
+  });
+});
+
+describe('reduce', () => {
+  it('should return single output value', async () => {
+    const numbers = [1, 2, 3];
+    const sum = (previous: number, current: number): number => previous + current;
+    const expected = numbers.reduce(sum);
+    const actual = await reduce(of(...numbers), sum, 0);
     expect(actual).to.equal(expected);
   });
 });
